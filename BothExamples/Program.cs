bbba5122c71eef05f025a83b9881b3cfc9a956dc
Sergey <melnikov_s_a@mail.ru>
2022-11-11 12:42:01 +0300

    Final perfection

--- BothExamples/Program.cs
+++ BothExamples/Program.cs
@@ -3,30 +3,29 @@
 // Пользователь вводит с клавиатуры M чисел.
 // Посчитайте, сколько чисел больше 0 ввёл пользователь.
 
-// Console.WriteLine("Здравствуйте. Введите числа через пробел");
+Console.WriteLine("Здравствуйте. Введите числа через пробел");
 
-// string userText = Console.ReadLine();
-// string[] userArray = userText.Split(" ");
+string userText = Console.ReadLine();
+string[] userArray = userText.Split(" ");
 
-// CountPositives(userArray);
+CountPositives(userArray);
 
-// void CountPositives(string[] text)
-// {
-//     int size = text.Length;
-//     int[] numbersArray = new int[size];
+void CountPositives(string[] text)
+{
+    int[] numbersArray = new int[text.Length];
 
-//     for (int i = 0; i < text.Length; i++)
-//     {
-//         numbersArray[i] = int.Parse(text[i]);
-//     }
+    for (int i = 0; i < text.Length; i++)
+    {
+        numbersArray[i] = int.Parse(text[i]);
+    }
 
-//     int count = 0;
-//     for (int j = 0; j < numbersArray.Length; j++)
-//     {
-//         if (numbersArray[j] > 0) count++;
-//     }
-//     Console.WriteLine($"Количество положительных = {count}");
-// }
+    int count = 0;
+    for (int j = 0; j < numbersArray.Length; j++)
+    {
+        if (numbersArray[j] > 0) count++;
+    }
+    Console.WriteLine($"Количество положительных = {count}");
+}
 
 // Напишите программу, которая найдёт точку пересечения двух прямых,
 // заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
@@ -39,27 +38,26 @@
 
 Console.WriteLine("Здравствуйте. Введите b1, k1, b2 и k2 через пробел");
 
-string userText = Console.ReadLine();
-string[] userArray = userText.Split(" ");
-double [] userNumbers = ReadNumbers (userArray);
+string userText2 = Console.ReadLine();
+string[] userArray2 = userText2.Split(" ");
+double [] userNumbers2 = ReadNumbers (userArray2);
 
-double b1 = userNumbers[0];
-double k1 = userNumbers[1];
-double b2 = userNumbers[2];
-double k2 = userNumbers[3];
+double b1 = userNumbers2[0];
+double k1 = userNumbers2[1];
+double b2 = userNumbers2[2];
+double k2 = userNumbers2[3];
 
 double x = (b2-b1)/(k1-k2);
-double y = k1 * x + b1;
+double y = k1 * x + b1; // незачем проверять вторую прямую - "y" заведомо равен
 Console.WriteLine($"Точка пересечения имеет координаты ({x},{y})");
 
-double[] ReadNumbers(string[] text)
+double[] ReadNumbers(string[] text2)
 {
-    int size = text.Length;
-    double[] numbersArray = new double[size];
+    double[] numbersArray2 = new double[text2.Length];
 
-    for (int i = 0; i < text.Length; i++)
+    for (int i = 0; i < text2.Length; i++)
     {
-        numbersArray[i] = double.Parse(text[i]);
+        numbersArray2[i] = double.Parse(text2[i]);
     }
-    return numbersArray;
+    return numbersArray2;
 }
